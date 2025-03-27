ed91328581c0090c58918b5bdb5d7503ba0e822e
XiangL <1786724721@qq.com>
2025-03-27 10:53:06 +0800

    吸顶导航

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,3 +1,4 @@
 <template>
     <div>woshi home</div>
+    <div style="height: 1200px;"></div>
 </template>
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,10 +1,12 @@
 <script setup>
 import LayoutNav from './components/LayoutNav.vue'
+import LayoutFixed from './components/LayoutFixed.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
 </script>
 
 <template>
+  <LayoutFixed/>
   <LayoutNav />
   <LayoutHeader />
   <RouterView />
