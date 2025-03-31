001235d0754788af40040bb4a86661837c488889
XiangL <1786724721@qq.com>
2025-03-31 20:10:05 +0800

    home-goodsItem组件封装

--- src/directives/index.js
+++ src/directives/index.js
@@ -7,11 +7,11 @@ export const lazyPlugin ={
             mounted(el, binding) {
                 // el:指令绑定的那个元素，在这里指img
                 // binding：bingding.value  指令等于号后面绑定的表达式的值  在这里指图片
-                console.log(el, binding.value);
+                // console.log(el, binding.value);
                const {stop}= useIntersectionObserver(
                     el,
                     ([{ isIntersecting }]) => {
-                      console.log(isIntersecting)
+                      // console.log(isIntersecting)
                       if (isIntersecting) {
                         // 进入视口区域
                         el.src = binding.value
--- src/views/Home/components/HomeProduct.vue
+++ src/views/Home/components/HomeProduct.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue';
 import HomePanel from './HomePanel.vue'
 import { getGoodsAPI } from '@/apis/home';
 import { onMounted } from 'vue';
+import GoodsItem from './GoodsItem.vue';
 
 // 获取数据列表
 const  goodsProduct =ref([])
@@ -31,12 +32,7 @@ onMounted(()=>{
         </RouterLink>
         <ul class="goods-list">
           <li v-for="good in cate.goods" :key="good.id">
-            <RouterLink to="/" class="goods-item">
-              <img v-img-lazy="good.picture" alt="" />
-              <p class="name ellipsis">{{ good.name }}</p>
-              <p class="desc ellipsis">{{ good.desc }}</p>
-              <p class="price">&yen;{{ good.price }}</p>
-            </RouterLink>
+            <GoodsItem :goods="good"/>
           </li>
         </ul>
       </div>
