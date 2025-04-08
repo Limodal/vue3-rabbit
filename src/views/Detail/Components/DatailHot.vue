80b5f6f854673b8b9aa7fd6acc6e36bbdda02e0f
XiangL <1786724721@qq.com>
2025-04-08 19:17:06 +0800

    详情热榜—title和列表内容的适配

--- src/views/Detail/Components/DatailHot.vue
+++ src/views/Detail/Components/DatailHot.vue
@@ -1,8 +1,27 @@
 <script setup>
 // 以24小时热榜获取数据渲染模板
 import { getHotGoodsAPI } from '@/apis/detail'
-import { onMounted, ref } from 'vue'
+import { onMounted, ref ,computed} from 'vue'
 import { useRoute } from 'vue-router'
+// 设计props参数  适配不同的title 和数据
+
+const props=defineProps({
+  hotType: {
+    type: Number
+  },
+})
+
+// 适配title 1.周日榜单 2.24小时热榜
+// const title = ref('')
+// if (props.hotType === 1) {
+//   title.value = '周日榜单'
+// } else if (props.hotType === 2) {
+//   title.value = '24小时热榜'
+// })
+// 这里也可以使用计算属性来实现
+const title = computed(() => {
+  return props.hotType === 1 ? '周日榜单' : '24小时热榜'})
+
 // 1.封装接口
 // 2.调用接口渲染模板
 
@@ -11,7 +30,7 @@ const hotList = ref([])
 const getHotList = async () => {
   const res = await getHotGoodsAPI({
     id: route.params.id,
-    type:1,
+    type: props.hotType,
   })
   hotList.value = res.result
 }
@@ -26,7 +45,7 @@ onMounted(() => {
 
 <template>
   <div class="goods-hot">
-    <h3>周日榜单</h3>
+    <h3>{{title}}</h3>
     <!-- 商品区块 -->
     <RouterLink to="/" class="goods-item" v-for="item in hotList" :key="item.id">
       <img :src="item.picture" alt="" />
