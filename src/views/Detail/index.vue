1b31da5c6e17918de12de22f0b540276eaf57d5c
XiangL <1786724721@qq.com>
2025-04-08 16:50:16 +0800

    详情-基础数据渲染

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -3,30 +3,33 @@ import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
 import { getDetail } from '@/apis/detail';
 
-const detailData = ref({});
-const route = useRoute();
-const getDetailData = async () => {
-  const res = await getDetail(route.params.id);
-  console.log(res);
-  detailData.value = res.result;
-};
-onMounted(() => {
-  getDetailData();
-});
+const goods = ref({})
+const route = useRoute()
+const getGoods = async () => {
+  const res = await getDetail(route.params.id)
+  goods.value = res.result
+}
+onMounted(() => getGoods())
 
 </script>
 
 <template>
   <div class="xtx-goods-page">
-    <div class="container">
+    <div class="container" v-if="goods.details">
       <div class="bread-container">
         <el-breadcrumb separator=">">
           <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-          <el-breadcrumb-item :to="{ path: '/' }">母婴
+          <!-- 
+          错误原因： goods一开始是个空对象{}  即首次渲染没有数据  {}.category-> undefined  ->undefined[1] 
+          解决方法
+          1. 通过v-if判断 goods.categories.length > 0
+          2. 可选链的语法 即 ?.  只有前面的数据有值才会继续访问后面的数据
+          -->
+          <el-breadcrumb-item :to="{ path: `/category/${goods.categories[1].id}` }">{{goods.categories[1].name}}
           </el-breadcrumb-item>
-          <el-breadcrumb-item :to="{ path: '/' }">跑步鞋
+          <el-breadcrumb-item :to="{ path: `/category/${goods.categories[0].id}` }">{{goods.categories[0].name}}
           </el-breadcrumb-item>
-          <el-breadcrumb-item>抓绒保暖，毛毛虫子儿童运动鞋</el-breadcrumb-item>
+          <el-breadcrumb-item>{{goods.name}}</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
       <!-- 商品信息 -->
@@ -40,33 +43,33 @@ onMounted(() => {
               <ul class="goods-sales">
                 <li>
                   <p>销量人气</p>
-                  <p> 100+ </p>
+                  <p> {{goods.salesCount}}+ </p>
                   <p><i class="iconfont icon-task-filling"></i>销量人气</p>
                 </li>
                 <li>
                   <p>商品评价</p>
-                  <p>200+</p>
+                  <p>{{ goods.commentCount }}+</p>
                   <p><i class="iconfont icon-comment-filling"></i>查看评价</p>
                 </li>
                 <li>
                   <p>收藏人气</p>
-                  <p>300+</p>
+                  <p>{{ goods.collectCount }}+</p>
                   <p><i class="iconfont icon-favorite-filling"></i>收藏商品</p>
                 </li>
                 <li>
                   <p>品牌信息</p>
-                  <p>400+</p>
+                  <p>{{goods.brand.name}}</p>
                   <p><i class="iconfont icon-dynamic-filling"></i>品牌主页</p>
                 </li>
               </ul>
             </div>
             <div class="spec">
               <!-- 商品信息区 -->
-              <p class="g-name"> 抓绒保暖，毛毛虫儿童鞋 </p>
-              <p class="g-desc">好穿 </p>
+              <p class="g-name"> {{goods.name}} </p>
+              <p class="g-desc">{{goods.desc}} </p>
               <p class="g-price">
-                <span>200</span>
-                <span> 100</span>
+                <span>{{goods.oldPrice}}</span>
+                <span> {{goods.newPrice}}</span>
               </p>
               <div class="g-service">
                 <dl>
@@ -106,13 +109,13 @@ onMounted(() => {
                 <div class="goods-detail">
                   <!-- 属性 -->
                   <ul class="attrs">
-                    <li v-for="item in 3" :key="item.value">
-                      <span class="dt">白色</span>
-                      <span class="dd">纯棉</span>
+                    <li v-for="item in goods.details.properties" :key="item.value">
+                      <span class="dt">{{item.name}}</span>
+                      <span class="dd">{{item.value}}</span>
                     </li>
                   </ul>
                   <!-- 图片 -->
-
+                    <img  v-for="img in goods.details.pictures" :src="img" :key="img" alt="" />
                 </div>
               </div>
             </div>
